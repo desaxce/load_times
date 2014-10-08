ca6140f394454956fb0ba21c9eb865751f3f06f3
Hugues <hugues.desaxce@orange.com>
2014-10-08 17:59:02 +0200

    Automatically tests over all protocols: h2, h2c, http and https!

--- test.cc
+++ test.cc
@@ -9,155 +9,164 @@ using namespace std;
 
 int main() {
 	
-	printf("Executing: rm -rf *log\n");
-	system("rm -rf *log");
-	printf("Executing: rm -rf ~/.cache/chromium\n");
-	system("rm -rf ~/.cache/chromium");
-
 	int set_incognito = 1;
 	int set_no_extensions = 1;
 	int set_ignore_certificate_errors = 1;
-	int set_enable_spdy4 = 1;
-	int is_secure = 0; // 1 implies TLS, 0 implies cleartext TCP
 	
 	// Time to wait for the webpage to load in seconds.
-	int sleep_time = 10;
+	int sleep_time = 20;
 
 	// Number of times you want to reach the webpage. The higher, the more
 	// accurate will be page loading times.
 	int times_to_reach = 1;
 
-	// Chromium executable.
-	// TODO: Fix the PATH to the executable
-	string chromium= "../src/out/Debug/chrome ";
+	int set_enable_spdy4 = 0;
+	int is_secure = 0; // 1 implies TLS, 0 implies cleartext TCP
 
-	// Chromium does not automatically connect to certain sites like
-	// google.fr; also chromium does not use caching.
-	string incognito = "--incognito ";
+	for (set_enable_spdy4 = 0; set_enable_spdy4 < 2; ++set_enable_spdy4) {
+		for (is_secure = 0; is_secure < 2; ++is_secure) {
 
-	// Removes annoying connection to 
-	// chrome://extensions/.../background_page.html.
-	string no_extensions = "--disable-extensions \
-		--disable-component-extensions-with-background-pages ";
+			printf("Executing: rm -rf *log\n");
+			system("rm -rf *log");
+			printf("Executing: rm -rf ~/.cache/chromium\n");
+			system("rm -rf ~/.cache/chromium");
 
-	// Skips when there is a non-recognized certificat. This can be 
-	// avoided if the server certificate is inserted in the browser
-	string ignore_certificate_errors = "--ignore-certificate-errors ";
 
-	// Enables the use of SPDY4 (HTTP/2)
-	string enable_spdy4 = "--enable-spdy4 ";
+			// Chromium executable.
+			// TODO: Fix the PATH to the executable
+			string chromium= "../src/out/Debug/chrome ";
 
-	// Always use HTTP/2 (SPDY4), and use it over TLS
-	string h2 = "--use-spdy=ssl ";
+			// Chromium does not automatically connect to certain sites like
+			// google.fr; also chromium does not use caching.
+			string incognito = "--incognito ";
 
-	// Always use HTTP/2 (SPDY4), and use it over TCP
-	string h2c = "--use-spdy=no-ssl ";
+			// Removes annoying connection to 
+			// chrome://extensions/.../background_page.html.
+			string no_extensions = "--disable-extensions \
+				--disable-component-extensions-with-background-pages ";
 
-	// Urls of the website on which we wish to test page loading times.
-	string scheme_http = "http://";
-	string scheme_https = "https://";
-	string scheme_used;
-	string port_http = ":80/";
-	string port_https = ":443/";
-	string port_used;
-	string ip_addr_localhost = "127.0.0.1";
-	string ip_addr_orange_server = "161.106.2.57";
-	string ip_addr_vps = "198.50.151.105";
-	string ip_addr_used = ip_addr_orange_server;
-	deque<string> urls;
-	//urls.push_back("leopard.html");
-	//urls.push_back("waves.html");
-	urls.push_back("index.html");
+			// Skips when there is a non-recognized certificat. This can be 
+			// avoided if the server certificate is inserted in the browser
+			string ignore_certificate_errors = "--ignore-certificate-errors ";
 
-	// We use the method std::to_string(), so  we need to add --std=c++0x
-	// in CXXFLAGS (see Makefile)
-	string sleep_cmd = "sleep " + to_string(sleep_time) + " ";
+			// Enables the use of SPDY4 (HTTP/2)
+			string enable_spdy4 = "--enable-spdy4 ";
 
-	// Kills last launched background process.
-	// TERM option used to "friendly" close an application.
-	string kill_last_bg_process = "kill -TERM $! ";
+			// Always use HTTP/2 (SPDY4), and use it over TLS
+			string h2 = "--use-spdy=ssl ";
 
-	// Command line to execute.
-	string command = chromium;
-	
-	if (set_incognito)
-		command += incognito;
-	if (set_no_extensions)
-		command += no_extensions;
-	if (set_ignore_certificate_errors)
-		command += ignore_certificate_errors;
-	if (set_enable_spdy4) {
-		command += enable_spdy4;
-		if (is_secure) {
-			command += h2;
-			scheme_used = scheme_https;
-			port_used = port_https;
-		}
-		else {
-			command += h2c;
-			scheme_used = scheme_http;
-			port_used = port_http;
-		}
-	}
-	else  {
-		if (is_secure) {
-			scheme_used = scheme_https;
-			port_used = port_https;
-		}
-		else {
-			scheme_used = scheme_http;
-			port_used = port_http;
-		}
-	}
+			// Always use HTTP/2 (SPDY4), and use it over TCP
+			string h2c = "--use-spdy=no-ssl ";
 
-	command += scheme_used + ip_addr_used + port_used;
+			// Urls of the website on which we wish to test page loading times.
+			string scheme_http = "http://";
+			string scheme_https = "https://";
+			string scheme_used;
+			string port_http = ":80/";
+			string port_https = ":443/";
+			string port_used;
+			string ip_addr_localhost = "127.0.0.1";
+			string ip_addr_orange_server = "161.106.2.57";
+			string ip_addr_vps = "198.50.151.105";
+			string ip_addr_used = ip_addr_vps;
+			deque<string> urls;
+			//urls.push_back("leopard.html");
+			//urls.push_back("waves.html");
+			urls.push_back("index.html");
 
-	for (deque<string>::const_iterator it = urls.begin(); it != urls.end();
-		 ++it) {
-		string new_command = command;
-		new_command += *it;
-		string log_file = *it + ".log";
-		string log1_file = *it + "1.log";
-		string log2_file = *it + "2.log";
+			// We use the method std::to_string(), so  we need to add --std=c++0x
+			// in CXXFLAGS (see Makefile)
+			string sleep_cmd = "sleep " + to_string(sleep_time) + " ";
 
-		// Log stderr to file
-		new_command += " >> " + log_file + " 2>&1 ";
-		
-		// Add a timer (sleep) until the page loads
-		new_command += "& " + sleep_cmd;
+			// Kills last launched background process.
+			// TERM option used to "friendly" close an application.
+			string kill_last_bg_process = "kill -TERM $! ";
 
-		// Kills the last launched background process (that is chromium!).
-		new_command += "&& " + kill_last_bg_process;
-
-		// Run the command |times_to_reach| times
-		for (int i = 0; i < times_to_reach; ++i) {
-			printf("%s\n", new_command.c_str());
-			system(new_command.c_str());
-		}
-
-		system(("egrep '(^setNavigationStart|^markLoadEventEnd)' " +
-				log_file + " > " + log1_file).c_str());
-		system(("cat " + log1_file + " | awk '{print $3}' > " +
-				log2_file).c_str());
-		
-		string line;	
-		ifstream myfile(log2_file);
-		if (myfile.is_open()) {
-			double loading_time = 0;
-
-			for (int i = 0; i < times_to_reach; ++i) {
-				getline(myfile, line);
-				double start = strtod(line.c_str(), NULL);
-				getline(myfile, line);
-				double end = strtod(line.c_str(), NULL);
-				loading_time += end-start;
+			// Command line to execute.
+			string command = chromium;
+			
+			if (set_incognito)
+				command += incognito;
+			if (set_no_extensions)
+				command += no_extensions;
+			if (set_ignore_certificate_errors)
+				command += ignore_certificate_errors;
+			if (set_enable_spdy4) {
+				command += enable_spdy4;
+				if (is_secure) {
+					command += h2;
+					scheme_used = scheme_https;
+					port_used = port_https;
+				}
+				else {
+					command += h2c;
+					scheme_used = scheme_http;
+					port_used = port_http;
+				}
 			}
-			myfile.close();
-			printf("Average loading time for %s = %f\n", log_file.c_str(), 
-				   loading_time/times_to_reach);
-		}
-		else {
-			cout << "Unable to open file";
+			else  {
+				if (is_secure) {
+					scheme_used = scheme_https;
+					port_used = port_https;
+				}
+				else {
+					scheme_used = scheme_http;
+					port_used = port_http;
+				}
+			}
+
+			command += scheme_used + ip_addr_used + port_used;
+
+			for (deque<string>::const_iterator it = urls.begin(); it != urls.end();
+				 ++it) {
+				string new_command = command;
+				new_command += *it;
+				string log_file = *it + ".log";
+				string log1_file = *it + "1.log";
+				string log2_file = *it + "2.log";
+
+				// Log stderr to file
+				new_command += " >> " + log_file + " 2>&1 ";
+				
+				// Add a timer (sleep) until the page loads
+				new_command += "& " + sleep_cmd;
+
+				// Kills the last launched background process (that is chromium!).
+				new_command += "&& " + kill_last_bg_process;
+
+				// Run the command |times_to_reach| times
+				for (int i = 0; i < times_to_reach; ++i) {
+					printf("%s\n", new_command.c_str());
+					system(new_command.c_str());
+				}
+
+				system(("egrep '(^setNavigationStart|^markLoadEventEnd)' " +
+						log_file + " > " + log1_file).c_str());
+				system(("cat " + log1_file + " | awk '{print $3}' > " +
+						log2_file).c_str());
+				
+				string line;	
+				ifstream myfile(log2_file);
+				if (myfile.is_open()) {
+					double loading_time = 0;
+
+					for (int i = 0; i < times_to_reach; ++i) {
+						getline(myfile, line);
+						double start = strtod(line.c_str(), NULL);
+						getline(myfile, line);
+						double end = strtod(line.c_str(), NULL);
+						loading_time += end-start;
+					}
+					myfile.close();
+					printf("For spdy4 = %d and is_secure = %d:\n", set_enable_spdy4, 
+						   is_secure);
+					printf("\tAverage loading time for %s = %f\n", 
+						   log_file.c_str(), loading_time/times_to_reach);
+				}
+				else {
+					cout << "Unable to open file";
+				}
+			}
 		}
 	}
 	return 0;
