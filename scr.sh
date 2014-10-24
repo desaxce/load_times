222cd8a7784fea14951b30991553531e81106437
Hugues <hugues.desaxce@orange.com>
2014-10-24 18:51:19 +0200

    Real tests now

--- scr.sh
+++ scr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for delay in 0 50 100 150 200 250 300
+for delay in 0 100 200 300 
 do
-	for losses in 0 1 2 3 4 5 6
+	for losses in 0 1 3 5 
 	do
-		./test --ip 172.20.36.138 -s 30 -v -t 1 -i eth2 -d $delay -l $losses -C yiping -r leopard.html
+		./test --ip 172.20.36.138 -s 60 -v -t 10 -i eth2 -d $delay -l $losses -C yiping -r leopard.html google/index.html youtube/index.html wikipedia/index.html laposte/index.html yahoo/index.html
 	done
 done
 
